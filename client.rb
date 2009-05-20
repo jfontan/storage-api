298fd6158fe260426be228f6db3f54ba618078d0
Javi Fontan <jfontan@gmail.com>
2009-05-20 17:01:00 +0200

    Client informs of the upload speed

--- client.rb
+++ client.rb
@@ -8,12 +8,15 @@ MB_DIV=1024*1024
 
 def upload_file(url, metadata, file)
     c = Curl::Easy.new(url)
+    
+    start_time=Time.now
 
     c.on_progress {|d_total, d_now, u_total, u_now|
-        print " %8.2f Mb / %8.2f Mb | %5.2f %%\r" % [
+        print " %8.2f Mb / %8.2f Mb | %5.2f %% | %5.2f Mb/s\r" % [
             u_now*1.0/MB_DIV, 
             u_total*1.0/MB_DIV,
-            u_now*1.0/u_total*100.0
+            u_now*1.0/u_total*100.0,
+            (u_now*1.0/MB_DIV)/(Time.now-start_time).to_i
         ]
         true
     }
@@ -23,6 +26,8 @@ def upload_file(url, metadata, file)
         Curl::PostField.content('metadata', metadata),
         Curl::PostField.file('file', file)
     )
+    puts
+        
     c.body_str
 end
 
